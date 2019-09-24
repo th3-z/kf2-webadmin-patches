d577ee3a25af8f01478e419dfd225d3f88d29dc2
the_z <the_z@znel.org>
2019-09-24 20:43:27 +0100

    Add patched flag

--- gen-patches.sh
+++ gen-patches.sh
@@ -4,3 +4,4 @@ mkdir -p patches
 
 git diff c96430ca8e5e57698a82738d0156fbfa2fc70843:ServerAdmin/gamesummary.inc ServerAdmin/gamesummary.inc > patches/gamesummary.inc.patch
 git diff c96430ca8e5e57698a82738d0156fbfa2fc70843:ServerAdmin/current_rules.inc ServerAdmin/current_rules.inc > patches/current_rules.inc.patch
+git diff c96430ca8e5e57698a82738d0156fbfa2fc70843:ServerAdmin/header_base.inc ServerAdmin/header_base.inc > patches/header_base.inc.patch
