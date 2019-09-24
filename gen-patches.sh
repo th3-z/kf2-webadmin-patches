a442e6b7b9d51511d8c582c3ee20de615dc1597d
the_z <the_z@znel.org>
2019-09-24 09:32:01 +0000

    Fix generator not finding files

--- gen-patches.sh
+++ gen-patches.sh
@@ -1,8 +1,6 @@
 #! /bin/bash
 
-mkdir -p ./patches
+mkdir -p patches
 
-cd ServerAdmin
-git diff c96430ca8e5e57698a82738d0156fbfa2fc70843:gamesummary.inc gamesummary.inc > ../patches/gamesummary.inc.patch
-git diff c96430ca8e5e57698a82738d0156fbfa2fc70843:current_rules.inc current_rules.inc > ../patches/current_rules.inc.patch
-cd ..
+git diff c96430ca8e5e57698a82738d0156fbfa2fc70843:ServerAdmin/gamesummary.inc ServerAdmin/gamesummary.inc > patches/gamesummary.inc.patch
+git diff c96430ca8e5e57698a82738d0156fbfa2fc70843:ServerAdmin/current_rules.inc ServerAdmin/current_rules.inc > patches/current_rules.inc.patch
