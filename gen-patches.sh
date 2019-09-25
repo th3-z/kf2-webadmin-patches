e693bd269e253f991e7e92a2689802a95476f533
the_z <the_z@znel.org>
2019-09-25 21:09:29 +0100

    Make paths relative

--- gen-patches.sh
+++ gen-patches.sh
@@ -2,6 +2,8 @@
 
 mkdir -p patches
 
-git diff c96430ca8e5e57698a82738d0156fbfa2fc70843:ServerAdmin/gamesummary.inc ServerAdmin/gamesummary.inc > patches/gamesummary.inc.patch
-git diff c96430ca8e5e57698a82738d0156fbfa2fc70843:ServerAdmin/current_rules.inc ServerAdmin/current_rules.inc > patches/current_rules.inc.patch
-git diff c96430ca8e5e57698a82738d0156fbfa2fc70843:ServerAdmin/header_base.inc ServerAdmin/header_base.inc > patches/header_base.inc.patch
+cd ServerAdmin
+git diff --relative c96430ca8e5e57698a82738d0156fbfa2fc70843:./gamesummary.inc gamesummary.inc > ../patches/gamesummary.inc.patch
+git diff --relative c96430ca8e5e57698a82738d0156fbfa2fc70843:./current_rules.inc current_rules.inc > ../patches/current_rules.inc.patch
+git diff --relative c96430ca8e5e57698a82738d0156fbfa2fc70843:./header_base.inc header_base.inc > ../patches/header_base.inc.patch
+cd ..
