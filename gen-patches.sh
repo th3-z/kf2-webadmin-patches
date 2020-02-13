2ef32a30d72ca3cf2456f20f2de1cf773aa60dc2
the_z <the_z@znel.org>
2020-02-13 22:25:42 +0000

    Add makefile, update paths

--- gen-patches.sh
+++ gen-patches.sh
@@ -2,14 +2,13 @@
 
 SEED_COMMIT=14cfe14c5f56304701eadfe7a0319c47cf60d3aa
 
-mkdir -p patches
+mkdir -p ./patches/Web/ServerAdmin
+mkdir -p ./patches/Web/images
 
-cd ServerAdmin
-git diff --relative $SEED_COMMIT:./gamesummary.inc gamesummary.inc > ../patches/gamesummary.inc.patch
-git diff --relative $SEED_COMMIT:./current_rules.inc current_rules.inc > ../patches/current_rules.inc.patch
-git diff --relative $SEED_COMMIT:./header_base.inc header_base.inc > ../patches/header_base.inc.patch
-git diff --relative $SEED_COMMIT:./current_players_row.inc current_players_row.inc > ../patches/current_players_row.inc.patch
-git diff --relative $SEED_COMMIT:./current_players.html current_players.html > ../patches/current_players.html.patch
-git diff --relative $SEED_COMMIT:./current_player_row.inc current_player_row.inc > ../patches/current_player_row.inc.patch
-git diff --relative $SEED_COMMIT:./current.html current.html > ../patches/current.html.patch
-cd ..
+git diff --relative $SEED_COMMIT:Web/ServerAdmin/gamesummary.inc -- Web/ServerAdmin/gamesummary.inc > patches/Web/ServerAdmin/gamesummary.inc.patch
+git diff --relative $SEED_COMMIT:Web/ServerAdmin/current_rules.inc -- Web/ServerAdmin/current_rules.inc > patches/Web/ServerAdmin/current_rules.inc.patch
+git diff --relative $SEED_COMMIT:Web/ServerAdmin/header_base.inc -- Web/ServerAdmin/header_base.inc > patches/Web/ServerAdmin/header_base.inc.patch
+git diff --relative $SEED_COMMIT:Web/ServerAdmin/current_players_row.inc -- Web/ServerAdmin/current_players_row.inc > patches/Web/ServerAdmin/current_players_row.inc.patch
+git diff --relative $SEED_COMMIT:Web/ServerAdmin/current_players.html -- Web/ServerAdmin/current_players.html > patches/Web/ServerAdmin/current_players.html.patch
+git diff --relative $SEED_COMMIT:Web/ServerAdmin/current_player_row.inc -- Web/ServerAdmin/current_player_row.inc > patches/Web/ServerAdmin/current_player_row.inc.patch
+git diff --relative $SEED_COMMIT:Web/ServerAdmin/current.html -- Web/ServerAdmin/current.html > patches/Web/ServerAdmin/current.html.patch
