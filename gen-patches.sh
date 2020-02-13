43d40e07bb70ac32af7af636530e66908d3547c8
the_z <the_z@znel.org>
2020-02-13 21:50:09 +0000

    Record seed commit

--- gen-patches.sh
+++ gen-patches.sh
@@ -1,13 +1,15 @@
 #! /bin/bash
 
+SEED_COMMIT=14cfe14c5f56304701eadfe7a0319c47cf60d3aa
+
 mkdir -p patches
 
 cd ServerAdmin
-git diff --relative c96430ca8e5e57698a82738d0156fbfa2fc70843:./gamesummary.inc gamesummary.inc > ../patches/gamesummary.inc.patch
-git diff --relative c96430ca8e5e57698a82738d0156fbfa2fc70843:./current_rules.inc current_rules.inc > ../patches/current_rules.inc.patch
-git diff --relative c96430ca8e5e57698a82738d0156fbfa2fc70843:./header_base.inc header_base.inc > ../patches/header_base.inc.patch
-git diff --relative c96430ca8e5e57698a82738d0156fbfa2fc70843:./current_players_row.inc current_players_row.inc > ../patches/current_players_row.inc.patch
-git diff --relative c96430ca8e5e57698a82738d0156fbfa2fc70843:./current_players.html current_players.html > ../patches/current_players.html.patch
-git diff --relative c96430ca8e5e57698a82738d0156fbfa2fc70843:./current_player_row.inc current_player_row.inc > ../patches/current_player_row.inc.patch
-git diff --relative c96430ca8e5e57698a82738d0156fbfa2fc70843:./current.html current.html > ../patches/current.html.patch
+git diff --relative $SEED_COMMIT:./gamesummary.inc gamesummary.inc > ../patches/gamesummary.inc.patch
+git diff --relative $SEED_COMMIT:./current_rules.inc current_rules.inc > ../patches/current_rules.inc.patch
+git diff --relative $SEED_COMMIT:./header_base.inc header_base.inc > ../patches/header_base.inc.patch
+git diff --relative $SEED_COMMIT:./current_players_row.inc current_players_row.inc > ../patches/current_players_row.inc.patch
+git diff --relative $SEED_COMMIT:./current_players.html current_players.html > ../patches/current_players.html.patch
+git diff --relative $SEED_COMMIT:./current_player_row.inc current_player_row.inc > ../patches/current_player_row.inc.patch
+git diff --relative $SEED_COMMIT:./current.html current.html > ../patches/current.html.patch
 cd ..
