9207ec128dd43274fbc3ad2cd2e4112b9cc8223b
Yakonche <60564904+Yakonche@users.noreply.github.com>
2020-02-13 22:36:04 +0100

    Add level and perk columns to Server Info and Players

--- gen-patches.sh
+++ gen-patches.sh
@@ -6,4 +6,8 @@ cd ServerAdmin
 git diff --relative c96430ca8e5e57698a82738d0156fbfa2fc70843:./gamesummary.inc gamesummary.inc > ../patches/gamesummary.inc.patch
 git diff --relative c96430ca8e5e57698a82738d0156fbfa2fc70843:./current_rules.inc current_rules.inc > ../patches/current_rules.inc.patch
 git diff --relative c96430ca8e5e57698a82738d0156fbfa2fc70843:./header_base.inc header_base.inc > ../patches/header_base.inc.patch
+git diff --relative c96430ca8e5e57698a82738d0156fbfa2fc70843:./current_players_row.inc current_players_row.inc > ../patches/current_players_row.inc.patch
+git diff --relative c96430ca8e5e57698a82738d0156fbfa2fc70843:./current_players.html current_players.html > ../patches/current_players.html.patch
+git diff --relative c96430ca8e5e57698a82738d0156fbfa2fc70843:./current_player_row.inc current_player_row.inc > ../patches/current_player_row.inc.patch
+git diff --relative c96430ca8e5e57698a82738d0156fbfa2fc70843:./current.html current.html > ../patches/current.html.patch
 cd ..
